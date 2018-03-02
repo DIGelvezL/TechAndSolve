6dff23984f2f713dca8ae53e27d3debf405ea3a4
INTERGRUPO\Dgelvez <dgelvez@intergrupo.com>
2018-03-02 11:15:47 -0500

    first commit

---
+++ src/main/java/com/solve/prueba/TechAndSolveApplication.java
@@ -0,0 +1,12 @@
+package com.solve.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechAndSolveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechAndSolveApplication.class, args);
+	}
+}
