cd8f91372dac99a88b034fc97593faf741b98009
INTERGRUPO\Dgelvez <dgelvez@intergrupo.com>
2018-03-08 17:02:40 -0500

    no message

--- src/main/java/com/solve/prueba/repository/VueloRepository.java
+++ src/main/java/com/solve/prueba/repository/VueloRepository.java
@@ -1,8 +1,5 @@
 package com.solve.prueba.repository;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +8,4 @@ import com.solve.prueba.model.entities.Vuelo;
 @Repository
 public interface VueloRepository extends CrudRepository<Vuelo, Integer>{
 
-	@Query(value = "SELECT v FROM Vuelo v ")
-	public List<Vuelo> consultarVuelos();
 }
--- src/main/java/com/solve/prueba/service/impl/ReservaServiceImpl.java
+++ src/main/java/com/solve/prueba/service/impl/ReservaServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 import com.solve.prueba.exception.ReservasException;
 import com.solve.prueba.model.dto.ReservaDto;
 import com.solve.prueba.model.dto.RespuestaDto;
+import com.solve.prueba.model.dto.VuelosDto;
 import com.solve.prueba.model.entities.Reserva;
 import com.solve.prueba.model.entities.Usuario;
 import com.solve.prueba.model.response.ReservarVueloResponse;
@@ -57,36 +59,26 @@ public class ReservaServiceImpl implements ReservaService {
 	
 	@Override
 	public List<ReservaDto> consultarByCedula(String cedula) throws ReservasException {
-		List<ReservaDto> reservaDtoList = new ArrayList<>();
 		
 		List<Reserva> reserva = reservaRepository.consultarByCedula(cedula);
 		
 		if(Objects.nonNull(reserva) && !reserva.isEmpty()){
-			for (Reserva item : reserva) {
-				ReservaDto reservaDto = modelMapper.map(item, ReservaDto.class);
-				
-				reservaDtoList.add(reservaDto);
-			}
+			return reserva.stream().map(r -> modelMapper.map(r, ReservaDto.class)).collect(Collectors.toList());
 		}
 		
-		return reservaDtoList;
+		return new ArrayList<>();
 	}
 	
 	@Override
 	public List<ReservaDto> consultarByCedulaFecha(String cedula, Date fecha) throws ReservasException {
-		List<ReservaDto> reservaDtoList = new ArrayList<>();
 		
 		List<Reserva> reserva = reservaRepository.consultarByCedulaFecha(cedula, fecha);
 		
 		if(Objects.nonNull(reserva) && !reserva.isEmpty()){
-			for (Reserva item : reserva) {
-				ReservaDto reservaDto = modelMapper.map(item, ReservaDto.class);
-				
-				reservaDtoList.add(reservaDto);
-			}
+			return reserva.stream().map(r -> modelMapper.map(r, ReservaDto.class)).collect(Collectors.toList());
 		}
 		
-		return reservaDtoList;
+		return new ArrayList<>();
 	}
 
 }
